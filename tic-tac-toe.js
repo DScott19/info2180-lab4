4bd844fd4907932cea9e146e46398243a6a4cc24
Deandra Scott <scott_deandra@yahoo.com>
2019-10-13 17:28:59 -0500

    Initial javascript file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,40 @@
 document.addEventListener("DOMContentLoaded",() => {
 
 	let divs = document.getElementById("board").querySelectorAll("div");
-	divs.forEach(myFunction);
+	divs.forEach(addSquare);
+	divs.forEach(addXorO);
 });	
 
-function myFunction (item){
+var AlternatePlay=[];
+
+
+function addSquare (item){
 	item.className="square";
 }
 
+function addX(playX){
+	playX.className="square X";
+	playX.innerHTML="X";
+		
+}
 
+function addO (playO){
+	playO.className="square O";
+	playO.innerHTML="O";
+
+}
+
+
+function addXorO(element){
+	element.addEventListener("click",function(){
+		if (AlternatePlay.length<1 || AlternatePlay[AlternatePlay.length-1]==="O"){
+				addX(element);
+				AlternatePlay.push("X");
+			
+		}	else{
+			AlternatePlay.push("O");
+			addO(element);
+		}
+	});
+
+}
